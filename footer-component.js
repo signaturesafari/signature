abe539af4823e7612eec4232adebe046186a5584
Salmin <salmin20shafii@gmail.com>
2024-07-13 14:35:53 +0300

    google analytic

---
+++ footer-component.js
@@ -0,0 +1,121 @@
+class FooterComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="css/animate.css">
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+  <link rel="stylesheet" href="css/flaticon.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap" rel="stylesheet">
+
+  <script src="https://kit.fontawesome.com/5fe73a6d63.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+      <footer class="ftco-footer" style="
+        background-image: linear-gradient(
+            45deg,
+            rgba(0, 0, 0, 0.459),
+            rgba(5, 5, 5, 0.459)
+          ),
+          url(images/animalbg.jpg);
+          backgound-position:center;
+          background-size:cover;
+          background-repeat: no-repeat;">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-md pt-5">
+              <div class="ftco-footer-widget pt-md-5 mb-4">
+                <h2 class="ftco-heading-2 text-white">About</h2>
+                <a class="navbar-brand bg-white mb-5 px-2 py-2" href="index.html">
+                  <img src="images/signature-safari-official-logo.png" height="100px" width="150px" />
+                </a>
+                <p class="text-white">Signature Safari is a sustainable and locally-owned tour operator in Tanzania, offering a personalized and comprehensive service at an affordable value. With a focus solely on Tanzania destinations, customers can trust Signature Safari's expertise to provide a world-class safari experience tailored to their unique needs and interests.</p>
+                <ul class="ftco-footer-social list-unstyled float-md-left float-lft text-white">
+                  
+                  <li><a href="https://www.linkedin.com/in/signature-safari-78184311b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9TO8MfqdQ%2ByGTR9dJT5DeQ%3D%3D" class="d-flex align-items-center justify-content-center" style="background-color: #0077B5;"><span class="icon fa fa-linkedin"></span></a></li>
+                  <li><a href="https://www.youtube.com/@signaturesafari" class="d-flex align-items-center justify-content-center" style="background-color: red;"><span class="icon fa fa-youtube"></span></a></li>
+                  <li><a href="https://www.facebook.com/signaturesafaritz" class="d-flex align-items-center justify-content-center" style="background-color: #1877F2;"><span class="icon fa fa-facebook"></span></a></li>
+                  <li><a href="https://www.instagram.com/signature_safari?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="d-flex align-items-center justify-content-center" style="background:linear-gradient( #f9ce34, #ee2a7b, #6228d7);"><span class="icon fa fa-instagram"></span></a></li>
+                  <li><a href="https://x.com/signaturestz" class="d-flex align-items-center justify-content-center" style="background-color: #000000;"><span class="icon fa fa-twitter"></span></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md pt-5 border-left">
+              <div class="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
+                <h2 class="ftco-heading-2 text-white">Important Link</h2>
+                <ul class="list-unstyled text-white">
+                  <li><a href="best_kilimanjaro_trekking_group_join_with_signature_safari_2023_2024_2025.html" class="py-2 d-block text-white">Kilimanjaro Joining Group</a></li>
+                  <li><a href="technical-clothing-for-mount-kilimanjaro-trek.html" class="py-2 d-block text-white">Technical Clothing for Mount Kilimanjaro</a></li>
+                  <li><a href="tanzania-safari-packing-list.html" class="py-2 d-block text-white">Tanzania Safari Packing List</a></li>
+                  <li><a href="technical-clothing-for-mount-kilimanjaro-trek.html" class="py-2 d-block text-white">Technical Clothing for Mt Kilimanjaro Clothing</a></li>
+                  <li><a href="tanzania-safari-packing-list.html" class="py-2 d-block text-white">Tanzania Safari Packaging List</a></li>
+                  <li><a href="sustainable-tourism-in-tanzania-with-signature-safari.html" class="py-2 d-block text-white">Sustainable Tourism in Tanzania</a></li>
+                  <li><a href="mt-kilimanjaro-and-meru-with-sar.html" class="py-2 d-block text-white">Kilimanjaro Search, Rescue and Safety Service</a></li>
+                  <li><a href="signature-safari-trustee-board.html" class="py-2 d-block text-white">Board of Trustees</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md pt-5 border-left">
+              <div class="ftco-footer-widget pt-md-5 mb-4">
+                <h2 class="ftco-heading-2 text-white">Stay in touch 24/7</h2>
+                <div class="block-23 mb-3">
+                  <ul class="text-white">
+                    <li><span class="icon fa fa-map-marker"></span><p class="text-white">Arusha, Tanzania</p></li>
+                    <li><a href="#"><span class="icon fa fa-whatsapp"></span><p class="text-white">+255 755 880 881</p></a></li>
+                    <li><a href="#"><span class="icon fa fa-whatsapp"></span><p class="text-white">+255 783 880 881</p></a></li>
+                    <li><a href="#"><span class="icon fa fa-paper-plane"></span><p class="text-white">info@signaturesafari.com</p></a></li>
+                  </ul>
+                  <div class="text-center mb-4">
+                    <h2 class="fw-bold text-white">Subscribe to our Newsletter</h2>
+                    <p class="text-white">Stay updated with our latest news and offers.</p>
+                  </div>
+                  <form>
+                    <div class="mb-3">
+                      <label for="name" class="form-label text-white">Name</label>
+                      <input type="text" class="form-control" id="name" placeholder="Enter your name" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="email" class="form-label text-white">Email address</label>
+                      <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
+                    </div>
+                    <button type="submit" class="btn text-white w-100" style="background-color:#f15d30;">Subscribe</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <h2 class="fw-bold text-white">We're happy to be trusted by</h2>
+              <p class="text-white>The biggest African Safari Booking platform <span class="bolding text-primary"><a class="text-primary" href="#">www.safarigo.com</a></span></p>
+            </div>
+          </div>
+          <div class="row mt-5">
+            <div class="col-md-12 text-center">
+              <p class="text-white">&copy; 2024 Signature Safari, All Right Reserved | Site by <a href="https://salmindeveloper.onrender.com/" class="text-primary">Salmin</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', FooterComponent);
---
+++ navbar-component.js
@@ -0,0 +1,213 @@
+class NavbarComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+      <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="css/animate.css">
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link rel="stylesheet" href="css/magnific-popup.css">
+      <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+      <link rel="stylesheet" href="css/jquery.timepicker.css">
+      <link rel="stylesheet" href="css/flaticon.css">
+      <link rel="stylesheet" href="css/style.css">
+      <link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+      <script src="https://kit.fontawesome.com/5fe73a6d63.js" crossorigin="anonymous"></script>
+
+      <nav class="navbar navbar-expand-lg navbar-light ftco_navbar ftco-navbar-light bg-warning bg-opacity-50" id="ftco-navbar">
+        <div class="container">
+          <a class="navbar-brand" href="index.html">
+            <img src="images/signature-safari-official-logo.png" height="90px" width="150px" />
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ftco-nav"
+            aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+          </button>
+          <div class="collapse navbar-collapse" id="ftco-nav">
+            <div class="row px-4 d-flex flex-column justify-content-center">
+              <ul class="navbar-nav px-3 justify-content-between border-bottom">
+                <span class="d-md-flex">
+                  <li class="nav-item d-none d-md-flex">
+                    <a href="contact.html" class="nav-link text-white">info@signaturesafari.com</a>
+                  </li>
+                  <li class="nav-item d-none d-md-flex">
+                    <a href="contact.html" class="nav-link text-white">+255 755 880 881</a>
+                  </li>
+                  <li class="nav-item">
+                    <div class="m-sm-0" id="google_translate_element"></div>
+                  </li>
+                </span>
+                <span class="d-flex">
+                  <li class="nav-item d-none d-md-flex">
+                    <a href="about-us-signaturesafari.html" class="nav-link text-white">About</a>
+                  </li>
+                  <li class="nav-item d-none d-md-flex">
+                    <a href="blog.html" class="nav-link text-white">Blog</a>
+                  </li>
+                  <li class="nav-item d-none d-md-flex">
+                    <a href="contact.html" class="nav-link text-white">Contact</a>
+                  </li>
+                </span>
+              </ul>
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a href="index.html" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item d-md-none d-block">
+                  <a href="about-us-signaturesafari.html" class="nav-link">About</a>
+                </li>
+                <li class="nav-item dropdown mega-menu">
+                  <a href="safari-packages.html" class="nav-link dropdown-toggle text-dark" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Safari</a>
+                  <div class="dropdown-menu bg-white mega-menu1" aria-labelledby="megaMenuDropdown">
+                    <div class="container w-100">
+                      <div class="row bg-warning w-100 d-flex justify-content-between align-items-center">
+                        <div class="col-md-3 col-sm-6 d-flex flex-column justify-content-center align-items-center w-100 py-3" style="
+                          height: 350px;
+                          background-image: linear-gradient(
+                              45deg,
+                              rgba(15, 15, 15, 0.612),
+                              rgba(15, 15, 15, 0.612)
+                            ),
+                            url('images/kilinp.jpg');
+                          background-position: center;
+                          background-repeat: no-repeat;
+                          background-size: cover;
+                        ">
+                          <h5 class="text-center text-white py-2 fw-bold">
+                            Tanzania Safari Packages
+                          </h5>
+                          <p class="text-white text-center">
+                            <a href="7-days-flying-beach-to-bush.html">7 Days Flying Beach to Bush</a>
+                          </p>
+                          <p class="text-center text-white">
+                            <a href="8-days-midrange-great-migration-safari.html">8 Days Midrange Great Migration Safari in Tanzania</a>
+                          </p>
+                          <button type="button" class="btn btn-outline-white mb-2 rounded-pill">
+                            <a href="tanzania-safari-packages.html">View all Packages</a>
+                          </button>
+                        </div>
+                        <div class="col-md-3 col-sm-6 d-flex flex-column justify-content-center align-items-center" style="
+                          height: 350px;
+                          background-image: linear-gradient(
+                              45deg,
+                              rgba(15, 15, 15, 0.612),
+                              rgba(15, 15, 15, 0.612)
+                            ),
+                            url('images/magical-kenya.jpg');
+                          background-position: center;
+                          background-repeat: no-repeat;
+                          background-size: cover;
+                          overflow: hidden;
+                        ">
+                          <h5 class="text-center text-white py-2 fw-bold">
+                            Kenya Safari Packages
+                          </h5>
+                          <p class="text-white text-center">
+                            <a href="5-days-masaimara-great-migration-safaris.html">5 Days Masai Mara Great Migration Safari</a>
+                          </p>
+                          <p class="text-center text-white">
+                            <a href="8-days-magical-kenya-luxury-safari.html">8 Days Magical Kenya Luxury Safari</a>
+                          </p>
+                          <button type="button" class="btn btn-outline-white mb-2 rounded-pill">
+                            <a href="kenya-safari-packages.html">View all Packages</a>
+                          </button>
+                        </div>
+                        <div class="col-md-3 col-sm-6 d-flex flex-column justify-content-center align-items-center" style="
+                          height: 350px;
+                          background-image: linear-gradient(
+                              45deg,
+                              rgba(15, 15, 15, 0.612),
+                              rgba(15, 15, 15, 0.612)
+                            ),
+                            url('images/uganda-destination.jpg');
+                          background-position: center;
+                          background-repeat: no-repeat;
+                          background-size: cover;
+                          overflow: hidden;
+                        ">
+                          <h5 class="text-center text-white py-2">
+                            Uganda Safari Packages
+                          </h5>
+                          <p class="text-center text-white">
+                            <a href="7-days-uganda-gorilla-and-chimpanzee-trekking-safari.html">7 Days Uganda Gorilla and Chimpanzee Trekking Safari</a>
+                          </p>
+                          <p class="text-center text-white">
+                            <a href="3-days-signature-safari-to-queen-elizabeth-national-park.html">3 Days Signature Safari to Queen Elizabeth National Park</a>
+                          </p>
+                          <button type="button" class="btn btn-outline-white mb-2 rounded-pill">
+                            <a href="uganda-safari-packages.html">View all Packages</a>
+                          </button>
+                        </div>
+                        <div class="col-md-3 col-sm-6 d-flex flex-column justify-content-center align-items-center" style="
+                          height: 350px;
+                          background-image: linear-gradient(
+                              45deg,
+                              rgba(15, 15, 15, 0.612),
+                              rgba(15, 15, 15, 0.612)
+                            ),
+                            url('images/mud.avif');
+                          background-position: center;
+                          background-repeat: no-repeat;
+                          background-size: cover;
+                          overflow: hidden;
+                        ">
+                          <button type="button" class="btn btn-outline-white mb-2 rounded-pill">
+                            <a href="safari-packages.html">View all Destinations</a>
+                          </button>
+                          <h5 class="text-white pt-3">SAFARI TIPS</h5>
+                          <p class="text-center text-white">
+                            <a href="safari-tips.html">What to pack for a Safari in Africa</a>
+                          </p>
+                          <p class="text-center text-white">
+                            <a href="safari-tips.html">Essential Tips to Plan Your African Safari</a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a href="luxury-lodge-safari.html" class="nav-link">Luxury Lodge Safari</a>
+                </li>
+                <li class="nav-item">
+                  <a href="honeymoon-safari.html" class="nav-link">Honeymoon Safari</a>
+                </li>
+                <li class="nav-item">
+                  <a href="blog.html" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="contact.html" class="nav-link">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    `;
+
+    // Load Bootstrap JavaScript within the Shadow DOM
+    const script = document.createElement('script');
+    script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+    script.onload = () => {
+      // Initialize Bootstrap features
+      const bootstrap = window.bootstrap;
+      new bootstrap.Collapse(this.shadowRoot.querySelector('.collapse'), {
+        toggle: false
+      });
+    };
+    this.shadowRoot.appendChild(script);
+  }
+}
+
+customElements.define('navbar-component', NavbarComponent);
+
+
+
